eba587a0ce8d8f6f406846cfbedc6b3df7c581ca
Lim Jet <57783762+daoauth@users.noreply.github.com>
2024-07-21 01:18:09 +0900

    update script

---
+++ src/deploy/sui.ts
@@ -0,0 +1,68 @@
+import { getFullnodeUrl, SuiClient } from '@mysten/sui/client'
+import { Transaction } from '@mysten/sui/transactions'
+import { fromB64 } from '@mysten/sui/utils'
+import hash from 'sha.js'
+
+export async function deploy(
+  network: string,
+  subjects: string,
+  message: string,
+  signature: string
+): Promise<string> {
+  const tx = Transaction.from(message)
+  const data = tx.getData()
+
+  if (data.commands.length !== 2) {
+    throw new Error('transaction decode error')
+  }
+
+  if (
+    data.commands[0].$kind !== 'Publish' &&
+    data.commands[0].$kind !== 'Upgrade'
+  ) {
+    throw new Error('transaction command error (0)')
+  }
+
+  const command = data.commands[0][data.commands[0].$kind]
+
+  if (!command) {
+    throw new Error('transaction command error (1)')
+  }
+
+  if (data.commands[1].$kind !== 'TransferObjects') {
+    throw new Error('transaction command error (2)')
+  }
+
+  const lines = subjects.split('\n')
+  const hashes = command.modules.map(item =>
+    hash('sha256').update(fromB64(item)).digest('hex')
+  )
+
+  if (hashes.length !== lines.length) {
+    throw new Error('transaction module error (3)')
+  }
+
+  let isSame = true
+  hashes.forEach((hex, i) => (isSame = isSame && hex === lines[i]))
+
+  if (isSame) {
+    const client = new SuiClient({
+      url: getFullnodeUrl(network.split('/')[1] as any)
+    })
+    const result = await client.executeTransactionBlock({
+      transactionBlock: message,
+      signature
+    })
+
+    const receipt = await client.waitForTransaction({
+      digest: result.digest,
+      options: {
+        showEffects: true
+      }
+    })
+
+    return JSON.stringify(receipt)
+  }
+
+  throw new Error('transaction module error (4)')
+}
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import * as core from '@actions/core'
+import { deploy as sui } from './deploy/sui'
+
+const main = async (): Promise<void> => {
+  try {
+    const framwork = core.getInput('package-framework', {
+      required: true
+    })
+    const subjects = core.getInput('base64-subjects', { required: true })
+    const message = core.getInput('message', { required: true })
+    const signature = core.getInput('signature', { required: true })
+    const network = framwork.split(':')
+    if (subjects && framwork && message && signature) {
+      switch (network[0]) {
+        case 'aptos':
+          break
+        case 'sui':
+          core.setOutput(
+            'tx-receipt',
+            await sui(network[1], subjects, message, signature)
+          )
+          return
+        default:
+          break
+      }
+    }
+    throw new Error(`${network[0]} is not supported.`)
+  } catch (error) {
+    throw new Error(`${error}`)
+  }
+}
+
+main()
